a463496ed6169964099456219e5fb7fd3b3519ad
RotichKipkoech <kipkoechrottich@gmail.com>
2023-07-01 03:35:19 +0300

    added code to populate the movie list

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ fetch('http://localhost:3000/films')
   .then(response => response.json())
   .then(data => {
     const films = data;
+
+      // Populate the movie list
+      const filmsList = document.getElementById('films');
+      films.forEach(film => {
+        const li = document.createElement('li');
+        li.classList.add('film', 'item');
+        li.textContent = film.title;
+        li.addEventListener('click', () => displayMovieDetails(film));
+        filmsList.appendChild(li);
+      });
